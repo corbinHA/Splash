3b058d14867843f8b9105c09e8d63cb509726fd4
Corbin Armendariz <corbinarmendariz@gmail.com>
2021-06-15 13:46:07 -0500

    final push

--- src/components/Links.js
+++ src/components/Links.js
@@ -1,11 +1,21 @@
 import React from 'react'
+import { useHistory } from 'react-router-dom'
 import './Links.css'
 
 function Links() {
+    
+    const history = useHistory();
+
+    const routeChange = () =>{ 
+        let path = 'https://grovergaming.com/about-us/'; 
+        history.push(path);
+    }
+
     return (
         <div className="links-wrapper">
             <div className="links-holder" >
-                <div className="orange-square"></div>
+                <button className="orange-square" onClick={routeChange}>
+                </button>
                 <div className="blue-square">Comic sans</div>
                 <div className="yellow-square">
                     <div className="text">
